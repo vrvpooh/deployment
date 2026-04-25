d30e0e06a037bf7e8a2245801c597faa70391302
voravee <voravee.nuc@dome.tu.ac.th>
2026-04-25 08:27:46 +0000

    bump to 2.0.1

--- api/main.go
+++ api/main.go
@@ -115,7 +115,7 @@ func healthReady(c *gin.Context) {
 func welcome(c *gin.Context) {
 	// TODO: เปลี่ยนข้อความนี้เพื่อทดสอบ CI/CD
 	jsonResponse(c, http.StatusOK, gin.H{
-		"message": "Hello from Go API v2.0.0 ✨",
+		"message": "Hello from Go API v2.0.1 ✨",
 		"time":    time.Now().UTC(),
 	})
 }
